de7a1d3195fd74a15cdb82125581564a3a65c568
anton <anton@LAPTOP-U74MQLQ5.mshome.net>
2021-01-03 13:20:22 +0100

    Create DBConnection

---
+++ Progetto/src/DBConnection/DBConnection.java
@@ -0,0 +1,51 @@
+package DBConnection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+	
+	private static DBConnection instance;
+	private Connection connection=null;
+	private final String USERNAME = "postgres";
+    private final String PASSWORD = "password";
+	private final String url = "jdbc:postgresql://progettomcking.cfwkm0wydepu.eu-west-3.rds.amazonaws.com:5432/postgres";
+	
+
+	
+	
+	
+	 private DBConnection() throws SQLException{
+		
+		try {
+			 Class.forName("org.postgresql.Driver");
+	         connection = DriverManager.getConnection(url, USERNAME, PASSWORD);
+	       
+		}
+		catch (Exception e) {
+			System.out.println("Errore di connesione ad DB");
+		}
+		
+	}
+	 
+	 public Connection getConnection() {
+		 return connection;
+	 }
+	 
+	 public static DBConnection getInstance() throws SQLException {
+	        if (instance == null)
+	        {
+	            instance = new DBConnection();
+	        }
+	        else
+	            if (instance.getConnection().isClosed())
+	            {
+	                instance = new DBConnection();
+	            }
+
+	        return instance;
+	  }
+	  
+
+}

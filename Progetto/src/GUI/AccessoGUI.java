a54e20fa4a6c663e6911b2226bd49fdf9040af76
anton <anton@LAPTOP-U74MQLQ5>
2021-01-03 19:02:07 +0100

    Insert AccessoGui and VisualizzaRistoranteGui

---
+++ Progetto/src/GUI/AccessoGUI.java
@@ -0,0 +1,62 @@
+package GUI;
+
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JButton;
+
+
+public class AccessoGUI extends JFrame {
+
+	private JPanel AccessoGUIPane;
+	
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					AccessoGUI frame = new AccessoGUI();
+					frame.setVisible(true);
+					frame.setResizable(false);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public AccessoGUI() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(550, 280, 450, 300);
+		AccessoGUIPane = new JPanel();
+		AccessoGUIPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(AccessoGUIPane);
+		AccessoGUIPane.setLayout(null);
+		
+		JLabel DomandaLable = new JLabel("Come desideri accedere?");
+		DomandaLable.setBounds(115, 10, 213, 84);
+		DomandaLable.setFont(new Font("Bell MT", Font.PLAIN, 18));
+		AccessoGUIPane.add(DomandaLable);
+		
+		JButton UtenteButton = new JButton("Utente");
+		UtenteButton.setBounds(125, 98, 158, 34);
+		UtenteButton.setFont(new Font("Bell MT", Font.PLAIN, 12));
+		AccessoGUIPane.add(UtenteButton);
+		
+		JButton AdminButton = new JButton("Admin");
+		AdminButton.setFont(new Font("Bell MT", Font.PLAIN, 12));
+		AdminButton.setBounds(125, 152, 158, 34);
+		AccessoGUIPane.add(AdminButton);
+		
+		JButton VisualizzaRistorantiButton = new JButton("Visualizza Ristoranti");
+		VisualizzaRistorantiButton.setFont(new Font("Bell MT", Font.PLAIN, 12));
+		VisualizzaRistorantiButton.setBounds(10, 232, 145, 21);
+		AccessoGUIPane.add(VisualizzaRistorantiButton);
+
+	}
+}
---
+++ Progetto/src/GUI/VisualizzaRistoranteGUI.java
@@ -0,0 +1,79 @@
+package GUI;
+
+
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import javax.swing.JComboBox;
+import javax.swing.JButton;
+
+
+public class VisualizzaRistoranteGUI extends JFrame {
+
+	private JPanel VisualizzaRistoranteGuiPane;
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					VisualizzaRistoranteGUI frame = new VisualizzaRistoranteGUI();
+					frame.setVisible(true);
+					frame.setResizable(false);				
+					
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+
+	JComboBox CittaComboBox;
+	
+	
+	public VisualizzaRistoranteGUI() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(550, 280, 450, 300);
+		VisualizzaRistoranteGuiPane = new JPanel();
+		VisualizzaRistoranteGuiPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(VisualizzaRistoranteGuiPane);
+		VisualizzaRistoranteGuiPane.setLayout(null);
+		
+		JLabel SceltaRistoranteLabel = new JLabel("Di quale ristorante vuoi avere informazioni?");
+		SceltaRistoranteLabel.setFont(new Font("Bell MT", Font.PLAIN, 18));
+		SceltaRistoranteLabel.setHorizontalAlignment(SwingConstants.LEFT);
+		SceltaRistoranteLabel.setBounds(43, 37, 364, 34);
+		VisualizzaRistoranteGuiPane.add(SceltaRistoranteLabel);
+		
+		String [] citta= {"","Napoli","Pisa","Verona"};
+		CittaComboBox = new JComboBox(citta);
+		CittaComboBox.setBounds(120, 81, 168, 21);
+		VisualizzaRistoranteGuiPane.add(CittaComboBox);
+				
+		JLabel InformazioniRistoranteLabel = new JLabel("//Far comparire le informazioni //");
+		InformazioniRistoranteLabel.setFont(new Font("Bell MT", Font.PLAIN, 12));
+		InformazioniRistoranteLabel.setBounds(43, 130, 245, 123);
+		VisualizzaRistoranteGuiPane.add(InformazioniRistoranteLabel);
+		
+		JButton CercaSullaMappaButton = new JButton("Cerca sulla mappa");
+		CercaSullaMappaButton.setFont(new Font("Bodoni MT", Font.PLAIN, 12));
+		CercaSullaMappaButton.setBounds(298, 212, 128, 29);
+		VisualizzaRistoranteGuiPane.add(CercaSullaMappaButton);
+		
+//		Mettere questo come azione nel Controller:
+//		Desktop desktop = Desktop.getDesktop();
+//		desktop.browse(new URI("https://www.google.it/maps/@40.853586,14.1729668,12z"));
+//
+//		Bisogna importare:
+//		import java.awt.Desktop;
+//		import java.net.URI;
+		
+	}
+}

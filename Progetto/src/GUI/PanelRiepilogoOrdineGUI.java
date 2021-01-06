a012a11265fdf4c648816aec5f533a607285175a
anton <anton@LAPTOP-U74MQLQ5>
2021-01-06 17:17:45 +0100

    Modifiche

---
+++ Progetto/src/GUI/PanelRiepilogoOrdineGUI.java
@@ -0,0 +1,14 @@
+package GUI;
+
+import javax.swing.JPanel;
+
+public class PanelRiepilogoOrdineGUI extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public PanelRiepilogoOrdineGUI() {
+
+	}
+
+}
--- Progetto/src/GUI/RiepilogoOrdineGUI.java
+++ Progetto/src/GUI/RiepilogoOrdineGUI.java
@@ -3,8 +3,13 @@ package GUI;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JButton;
+import java.awt.Color;
 
-public class RiepilogoOrdineGUI extends MediumFrame {
+public class RiepilogoOrdineGUI extends BigFrame {
 
 	/**
 	 * Launch the application.
@@ -26,9 +31,45 @@ public class RiepilogoOrdineGUI extends MediumFrame {
 	 * Create the frame.
 	 */
 	public RiepilogoOrdineGUI() {
+		getBodyPanel().setLocation(0, 30);
 		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		getBodyPanel().setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(10, 444, 880, 48);
+		getBodyPanel().add(panel);
+		panel.setLayout(null);
+		
+		JLabel prezzoTotaleLabel = new JLabel("Prezzo totale:");
+		prezzoTotaleLabel.setFont(new Font("Bell MT", Font.PLAIN, 16));
+		prezzoTotaleLabel.setBounds(10, 10, 98, 31);
+		panel.add(prezzoTotaleLabel);
+		
+		JLabel valorePrezzoTotaleLabel = new JLabel("");
+		valorePrezzoTotaleLabel.setFont(new Font("Bell MT", Font.PLAIN, 16));
+		valorePrezzoTotaleLabel.setBounds(724, 10, 146, 31);
+		panel.add(valorePrezzoTotaleLabel);
+		
+		JButton annullaButton = new JButton("Annulla");
+		annullaButton.setBackground(Color.WHITE);
+		annullaButton.setForeground(Color.BLACK);
+		annullaButton.setBorderPainted(false);
+		annullaButton.setFocusPainted(false);
+		annullaButton.setFocusTraversalKeysEnabled(false);
+		annullaButton.setFocusable(false);
+		annullaButton.setBounds(10, 515, 113, 21);
+		getBodyPanel().add(annullaButton);
+		
+		JButton confermaButton = new JButton("Conferma");
+		confermaButton.setBackground(Color.WHITE);
+		confermaButton.setForeground(Color.BLACK);
+		confermaButton.setBorderPainted(false);
+		confermaButton.setFocusPainted(false);
+		confermaButton.setFocusTraversalKeysEnabled(false);
+		confermaButton.setFocusable(false);
+		confermaButton.setBounds(786, 515, 104, 21);
+		getBodyPanel().add(confermaButton);
 
 	}
-
 }

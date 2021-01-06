358acb4c4b4475c563644cc2aa0e8725796c49fb
Ftkris_ <57858883+maammt@users.noreply.github.com>
2021-01-06 17:29:08 +0100

    Correzioni

--- Progetto/src/GUI/MediumFrame.java
+++ Progetto/src/GUI/MediumFrame.java
@@ -20,9 +20,7 @@ public class MediumFrame extends JFrame {
 	private JPanel contentPane;
 	private JPanel bodyPanel;
 	private JButton exitButton;
-
-	/**
-	 * Launch the application.
+	/* Launch the application.
 	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -37,36 +35,35 @@ public class MediumFrame extends JFrame {
 		});
 	}
 
-	/**
-	 * Create the frame.
+	/* Create the frame.
 	 */
 	public MediumFrame() {
 		super();
 		setResizable(false);
 		setUndecorated(true);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(550, 280, 900, 600);
+		setBounds(550, 280, 675, 450);
 		setLocationRelativeTo(null);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
-		
+
+
 		JPanel titoloPanel = new JPanel();
-		titoloPanel.setBounds(0, 0, 900, 46);
+		titoloPanel.setBounds(0, 0, 675, 46);
 		titoloPanel.setBackground(new Color(255, 0, 0));
 		contentPane.add(titoloPanel);
 		titoloPanel.setLayout(null);
-		
+
 		JLabel titoloLabel = new JLabel("McKING");
 		titoloLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		titoloLabel.setForeground(new Color(255, 255, 50));
 		titoloLabel.setFont(new Font("Papyrus", Font.BOLD, 26));
-		titoloLabel.setBounds(311, 0, 282, 46);
+		titoloLabel.setBounds(190, 0, 300, 46);
 		titoloPanel.add(titoloLabel);
-		
-		exitButton = new JButton("New button");
+
+		exitButton = new JButton("");
 		exitButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				dispose();
@@ -76,21 +73,20 @@ public class MediumFrame extends JFrame {
 		exitButton.setFocusPainted(false);
 		exitButton.setFocusable(false);
 		exitButton.setIcon(new ImageIcon(SmallFrame.class.getResource("/GUI/ExitIcon.png")));
-		exitButton.setBackground(null);		
+		exitButton.setBackground(null);    
 		exitButton.setFont(exitButton.getFont().deriveFont(0f));
-		exitButton.setBounds(855, 13, 22, 20);
+		exitButton.setBounds(645, 10, 20, 20);
 		exitButton.setBorder(null);
 		titoloPanel.add(exitButton);
-		
+
 		bodyPanel = new JPanel();
 		bodyPanel.setBackground(new Color(255, 255, 153));
-		bodyPanel.setBounds(0, 44, 900, 546);
-		contentPane.add(bodyPanel);	
-		
+		bodyPanel.setBounds(0, 44, 675, 410);
+		contentPane.add(bodyPanel);    
 	}
 
 	public JPanel getBodyPanel() {
 		return bodyPanel;
 	}
-	
+
 }

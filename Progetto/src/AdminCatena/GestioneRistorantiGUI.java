80c639eaf56df3199681be205597cc097475b6fb
Ftkris_ <57858883+maammt@users.noreply.github.com>
2021-01-09 10:42:03 +0100

    .

--- Progetto/src/AdminCatena/GestioneRistorantiGUI.java
+++ Progetto/src/AdminCatena/GestioneRistorantiGUI.java
@@ -1,9 +1,15 @@
 package AdminCatena;
 
 import java.awt.EventQueue;
+import java.awt.Font;
 
 import General.VisualizzaRistoranteGUI;
 import Utility.MediumFrame;
+import java.awt.GridLayout;
+import java.awt.GridBagLayout;
+import net.miginfocom.swing.MigLayout;
+import javax.swing.JLabel;
+import javax.swing.JComboBox;
 
 public class GestioneRistorantiGUI extends MediumFrame {
 
@@ -22,6 +28,20 @@ public class GestioneRistorantiGUI extends MediumFrame {
 		});
 	}
 	public GestioneRistorantiGUI() {
+		getBodyPanel().setLayout(new MigLayout("", "[][183.00][][191.00][184.00][]", "[][35.00][53.00]"));
+		
+		JLabel visualizzaLabel = new JLabel("Seleziona il ristorante");
+		visualizzaLabel.setFont(new Font("Bell MT", Font.BOLD, 16));
+		getBodyPanel().add(visualizzaLabel, "cell 1 1");
+		
+		JComboBox comboBox = new JComboBox();
+		getBodyPanel().add(comboBox, "cell 3 1,growx");
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		getBodyPanel().add(lblNewLabel, "cell 5 1");
+		
+		JLabel lblNewLabel_1 = new JLabel("New label");
+		getBodyPanel().add(lblNewLabel_1, "cell 1 2,alignx right");
 		
 	}
 
